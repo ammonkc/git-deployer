20796d016240e389565dde3468a3271fef3350b6
Ammon Casey <ammonkc@gmail.com>
2011-03-21 14:41:50 -1000

    deploy test

--- example/tasks.rb
+++ example/tasks.rb
@@ -6,7 +6,20 @@ $:.unshift(File.join(File.dirname(__FILE__), "..", "tasks"))
 
 load "git-deployer.nake"
 
-Task["deployer:setup"].config[:user] = "TODO"
-Task["deployer:setup"].config[:host] = "TODO"
-Task["deployer:setup"].config[:path] = "/var/www/deploy-test"
+#Task["deployer:setup"].config[:user] = "TODO"
+#Task["deployer:setup"].config[:host] = "TODO"
+#Task["deployer:setup"].config[:path] = "/var/www/deploy-test"
 
+begin
+  load "git-deployer.nake"
+  Task["deployer:setup"].config[:servers] = {
+    server1: {
+      user: "git",
+      host: "10.4.87.218",
+      repo: "/srv/git/deploy-test.git",
+      path: "/srv/www/codeigniter/application/deploy-test"
+    }
+  }
+rescue LoadError
+  warn "You have to install git-deployer gem if you want to deploy to remote servers!"
+end
